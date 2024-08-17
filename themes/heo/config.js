39dac6566b16d68001e654fd8b7ad64646ae6da8
John Kindem <461425614@qq.com>
2024-08-17 13:38:29 +0800

    feat: update personal info

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2024-08-17', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://www.flyandnotdown.com' },
+    { title: '🎉 开源游戏引擎 Explosion 开发中，欢迎加入', url: 'https://github.com/ExplosionEngine/Explosion' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -15,20 +15,20 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '分享图形编程',
+  HEO_HERO_TITLE_2: '与游戏人日常',
+  HEO_HERO_TITLE_3: 'FLYANDNOTDOWN.COM',
+  HEO_HERO_TITLE_4: 'NEW',
+  HEO_HERO_TITLE_5: '博客上新',
+  HEO_HERO_TITLE_LINK: '/tag/latest',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/recommend' },
+  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/hot' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/tutorial' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '推荐',
+  HEO_HERO_RECOMMEND_POST_TAG: 'recommend',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
@@ -36,86 +36,115 @@ const CONFIG = {
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
     '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
     '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
   ],
 
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/FlyAndNotDown',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  HEO_INFO_CARD_URL3: '/links',
+  HEO_INFO_CARD_TEXT3: '交换友链',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
     {
-      title_1: 'AfterEffect',
-      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
-      color_1: '#989bf8',
-      title_2: 'Sketch',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
-      color_2: '#ffffff'
-    },
-    {
-      title_1: 'Docker',
-      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
-      color_1: '#57b6e6',
-      title_2: 'Photoshop',
-      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
-      color_2: '#4082c3'
-    },
-    {
-      title_1: 'FinalCutPro',
-      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
+      title_1: 'Unreal',
+      img_1: '/images/heo/unreal.png',
       color_1: '#ffffff',
-      title_2: 'Python',
-      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
+      title_2: 'C++',
+      img_2: '/images/heo/cpp.svg',
       color_2: '#ffffff'
     },
     {
-      title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
-      color_1: '#eb6840',
-      title_2: 'Principle',
-      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
-      color_2: '#8f55ba'
-    },
-    {
-      title_1: 'illustrator',
-      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
-      color_1: '#f29e39',
-      title_2: 'CSS3',
-      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
-      color_2: '#2c51db'
-    },
-    {
-      title_1: 'JS',
-      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
-      color_1: '#f7cb4f',
-      title_2: 'HTML',
-      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
-      color_2: '#e9572b'
+      title_1: 'JavaScript',
+      img_1: '/images/heo/javascript.webp',
+      color_1: '#f7df1e',
+      title_2: 'PyCharm',
+      img_2: '/images/heo/pycharm.png',
+      color_2: '#ffffff'
     },
     {
       title_1: 'Git',
-      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
+      img_1: '/images/heo/git.png',
       color_1: '#df5b40',
-      title_2: 'Rhino',
-      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
-      color_2: '#1f1f1f'
+      title_2: 'CLion',
+      img_2: '/images/heo/clion.png',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'Vim',
+      img_1: '/images/heo/vim.png',
+      color_1: '#ffffff',
+      title_2: 'VSCode',
+      img_2: '/images/heo/vscode.png',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'Rider',
+      img_1: '/images/heo/rider.png',
+      color_1: '#ffffff',
+      title_2: 'Notion',
+      img_2: '/images/heo/notion.png',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'AndroidStudio',
+      img_1: '/images/heo/as.png',
+      color_1: '#ffffff',
+      title_2: 'Blender',
+      img_2: '/images/heo/blender.png',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'WebStorm',
+      img_1: '/images/heo/webstorm.png',
+      color_1: '#ffffff',
+      title_2: 'DX12',
+      img_2: '/images/heo/dx12.png',
+      color_2: '#000000'
+    },
+    {
+      title_1: 'Python',
+      img_1: '/images/heo/python.svg',
+      color_1: '#ffffff',
+      title_2: 'Explosion',
+      img_2: '/images/heo/explosion.png',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'Vulkan',
+      img_1: '/images/heo/vulkan.svg',
+      color_1: '#ffffff',
+      title_2: 'OpenGL',
+      img_2: '/images/heo/opengl.png',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'CMake',
+      img_1: '/images/heo/cmake.png',
+      color_1: '#ffffff',
+      title_2: 'VisualStudio',
+      img_2: '/images/heo/vs.png',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'Vercel',
+      img_1: '/images/heo/vercel.png',
+      color_1: '#ffffff',
+      title_2: 'GitHub',
+      img_2: '/images/heo/github.png',
+      color_2: '#ffffff'
     }
   ],
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: '',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
